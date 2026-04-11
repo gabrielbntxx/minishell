828ef119c526bfb9e446ed3727d3ca2107abd9f7
axododo <guilbertmael@gmail.com>
2026-04-11 05:04:55 +0200

    ajout de la struct env et includes

--- Includes/minishell.h
+++ Includes/minishell.h
@@ -1,1 +1,18 @@
-//struct, defines, prototypes logique quoi
+#ifndef MINISHELL_H
+  #define MINISHELL_H
+
+  # include <stdlib.h>
+  # include <unistd.h>
+  #include <sys/wait.h>
+
+  #include <stdio.h>
+
+
+typedef struct s_env {
+  char *key;
+  char *value;
+  struct s_env *next;
+} t_env;
+
+
+#endif
