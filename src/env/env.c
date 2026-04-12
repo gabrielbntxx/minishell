26a8c00d75eb693feffa6fe0c39ac0c2872bc307
axododo <guilbertmael@gmail.com>
2026-04-12 22:15:15 +0200

    ajout de env_set et env_get rien a signaler a part la norme et la protection des malloc a corriger

--- src/env/env.c
+++ src/env/env.c
@@ -2,17 +2,39 @@
 #include "../../Includes/minishell.h"
 #include "../res/res.h"
 
-char *env_get(t_env *env, char *key) {
+
+
+void *env_get(t_env *env, char *key, int option) {
   char *value;
 
   while (env) {
-    if (ft_strcmp(env->key, key) == 0)
+    if (ft_strcmp(env->key, key) == 0) {
+      if (option == 1) return(env);
       return(env->value);
+    }
     env = env->next;
   }
-  return (NULL);
+  if (option == 1) return(NULL);
+  return ("dont exist\n");
 }
 
+void env_set(t_env **env, char *key, char *value) {
+  t_env *node;
+
+  node = env_get(*env, key, 1);
+  if (!node) {
+    while ((*env)->next != NULL) {
+      *env = (*env)->next;
+    }
+    (*env)->next = malloc(sizeof(t_env));
+    *env = (*env)->next;
+    (*env)->key = key;
+    (*env)->value = value;
+    return;
+  }
+  (*env)->next = node;
+  *env = node;
+}
 
 void init_env(char **envp, t_env **first) {
   t_env *node;
@@ -48,7 +70,9 @@ int    main(int ac, char **av, char **envp)
   t_env *env;
   int i = 0;
   init_env(envp, &env);
-  char *value = env_get(env, "PWD");
+  char *value = env_get(env, "TEST", 0);
   printf("%s\n", value);
-  printf("%s\n", getenv("PWD"));
+  env_set(&env, "TEST", "hello");
+  value = env_get(env, "TEST", 0);
+  printf("%s\n", value);
 }
