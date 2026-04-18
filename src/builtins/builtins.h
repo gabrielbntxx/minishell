0e27bd7957280419aaa850be05afdc5b4e6241ac
axododo <guilbertmael@gmail.com>
2026-04-18 19:15:14 +0200

    mise a jour proto

--- src/builtins/builtins.h
+++ src/builtins/builtins.h
@@ -6,7 +6,7 @@
 char **sort_array(char **env);
 char **env_to_export(t_env *env);
 void print_export(char **env);
-void ft_export(char **env);
+void ft_export(char **cmd, t_env nodenv);
 int main(int ac, char **av, char **envp);
 
 #endif
