2ce992a12f9383b1fc40a833c38d351e5d289bf0
axododo <guilbertmael@gmail.com>
2026-04-21 16:56:30 +0200

    ajout exec et env dans main de test

--- src/main.c
+++ src/main.c
@@ -4,26 +4,32 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-int main(void)
-{
-	char	*input;
-	t_token	*tokens;
-	t_cmd	*cmds;
 
+int	main(int ac, char **av, char **envp)
+{
+	t_env	*env;
+	t_token	*tokens;
+	char	*cmd;
+	char	**array;
+
+	(void)ac;
+	(void)av;
+	env = NULL;
+	init_env(envp, &env);
+	array = env_to_array(env);
 	while (1)
 	{
-		input = readline("minishell> ");
-		if (!input)
+		cmd = readline("minishell> ");
+		if (!cmd)
 			break ;
-		if (*input)
-			add_history(input);
-		tokens = lexer(input);
-		printf("--- TOKENS ---\n");
+		if (*cmd)
+			add_history(cmd);
+		tokens = lexer(cmd);
+		printf("---- LEXER ----\n");
 		print_tokens(tokens);
-		cmds = parser(tokens);
-		printf("--- CMDS ---\n");
-		print_cmds(cmds);
-		free(input);
+		printf("---- EXEC ----\n");
+		execute_cmd(cmd, array);
+		free(cmd);
 	}
 	return (0);
 }
