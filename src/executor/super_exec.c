46b46c314cdc76e51dd2fae33f6dd21b856b0551
axododo <guilbertmael@gmail.com>
2026-04-30 16:32:28 +0200

    parre en couille au moindre |

--- src/executor/super_exec.c
+++ src/executor/super_exec.c
@@ -1,20 +1,36 @@
 #include "../../Includes/env.h"
 #include "../../Includes/executor.h"
 #include "../../Includes/minishell.h"
+#include <stdio.h>
+
 
 void super_exec(t_cmd *cmd, t_env *env) {
   char **array; 
   t_cmd *current;
-  int *pipe_fd;
-  int last_fd;
+  int *pipe_fd; // pipe_fd[0] = in, pipe_fd[1] = out,
+  int last_fd; // save pour la prochaine commande
   int pid;
 
   current = cmd;
   array = env_to_array(env);
   if (current->next) {
     while (current) {
-      
+      if (current->next) pipe(pipe_fd);
+      pid = fork();
+        if (pid == 0) {
+        if (last_fd != -1) dup2(last_fd, STDIN_FILENO);
+        if (current->next) { 
+        dup2(pipe_fd[1], STDOUT_FILENO);
+        close(pipe_fd[0]);
+        close(pipe_fd[1]);
+        execute_cmd(cmd, array);
+        }
+      }
+      close(pipe_fd[1]);
+      last_fd = pipe_fd[0];
+      current = current->next;
     }
+    while(wait(NULL) < 0);
   }
   else {
     if (dispatch(cmd, &env) == 1)
