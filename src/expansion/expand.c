09724a9da722133c875c5c7c43ac9aef85f6762d
axododo <guilbertmael@gmail.com>
2026-05-26 15:36:18 +0200

    test

--- src/expansion/expand.c
+++ src/expansion/expand.c
@@ -1,6 +1,14 @@
 #include "../../Includes/expand.h"
 
-
+int	ft_isalnum(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))
+	{
+		return (8);
+	}
+	else
+		return (0);
+}
 
 int	ft_strlcat(char *dst, const char *src, int size)
 {
@@ -30,20 +38,30 @@ int	ft_strlcat(char *dst, const char *src, int size)
 void expand(t_cmd *cmd, t_env *env) {
     int i;
     int y;
+    int end; 
     char *value; 
+    char *tmp; 
 
-
+    tmp = NULL; 
     i = 0;
+    end = 0;
     y = 0;
     while (cmd->args[i]) {
         if (ft_strchr(cmd->args[i], '$') != -1) {
             y = ft_strchr(cmd->args[i], '$');
-            if (cmd->args[i][y] == '?') {
-                return; //exit value
+            end = y + 1; 
+            ft_strlcpy(tmp, cmd->args[i], y);
+            if (cmd->args[i][y + 1] == '?') {
+                return; //exit value  
             }
+            while(ft_isalnum(cmd->args[i][end]))
+              end++;
             value = env_get(env, &cmd->args[i][y + 1], 0);
             if (!value) return;
-            ft_strlcpy(&cmd->args[i][y], value, ft_strlen(value) + 1);
+            cmd->args[i] = ft_strjoin(tmp, value);
+            //if (end - 1 > (int)ft_strlen(cmd->args[i]))
+              //cmd ->args[i] = ft_strjoin(cmd->args[i], ft_substr(cmd->args[i], y + 1, end));
+            //ft_strlcpy(&cmd->args[i][y], value, ft_strlen(value) + 1);
         }
         i++;
     }   
