f2a8deedbd610bf00ddd0acc2bdf6614b957ee2b
gabrielbenetrix <121184114+gabrielbntxx@users.noreply.github.com>
2026-07-14 00:08:09 +0200

    fix(cd): PWD absolu, cd --, cd - affiche, message avec le chemin
    - PWD est mis a jour via getcwd() apres chdir : 'cd ../../' stockait
      litteralement '../../' dans PWD (OLDPWD et cd - devenaient faux)
    - cd -- va au HOME comme bash ; cd - affiche le nouveau dossier
    - le message d'erreur inclut le chemin fautif :
      'minishell: cd: doesntexist: No such file or directory'

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -14,37 +14,48 @@
 #include "../../Includes/minishell.h"
 #include "../../Includes/parser.h"
 
-int	builtin_cd(t_env *env, char **args)
+static char	*cd_target(t_env *env, char **args)
 {
 	char	*path;
 
-	if (args[1] == NULL)
+	if (args[1] == NULL || !ft_strcmp(args[1], "--"))
 	{
 		path = env_get(env, "HOME", 0);
 		if (path == NULL)
-		{
 			write(2, "minishell: cd: HOME not set\n", 28);
-			return (1);
-		}
+		return (path);
 	}
-	else
-		path = args[1];
-	if (args[1] && !ft_strcmp(args[1], "-"))
+	if (!ft_strcmp(args[1], "-"))
 	{
 		path = env_get(env, "OLDPWD", 0);
 		if (!path || !path[0])
 		{
 			write(2, "minishell: cd: OLDPWD not set\n", 30);
-			g_exit_st = 1;
-			return (1);
+			return (NULL);
 		}
+		ft_putstr_fd(path, 1);
+		write(1, "\n", 1);
+		return (path);
 	}
+	return (args[1]);
+}
+
+int	builtin_cd(t_env *env, char **args)
+{
+	char	*path;
+	char	buff[1024];
+
+	path = cd_target(env, args);
+	if (!path)
+		return (1);
 	if (chdir(path) == -1)
 	{
-		perror("minishell: cd");
+		write(2, "minishell: cd: ", 15);
+		perror(path);
 		return (1);
 	}
 	env_set(&env, "OLDPWD", env_get(env, "PWD", 0));
-	env_set(&env, "PWD", path);
+	if (getcwd(buff, 1024) != NULL)
+		env_set(&env, "PWD", buff);
 	return (0);
 }
